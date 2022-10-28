538b3a8bdb61655d8708972f3841f35865fadf32
Noble Nwachukwu <ncnwachukwu@mix.wvu.edu>
2022-10-27 23:39:13 -0500

    added new codewars challenge

---
+++ 6kyu/duplicate-encoder.js
@@ -0,0 +1,29 @@
+// The goal of this exercise is to convert a string to a new string where each character in the new string is "(" if that character appears only once in the original string, or ")" if that character appears more than once in the original string. Ignore capitalization when determining if a character is a duplicate.
+
+// Examples
+// "din"      =>  "((("
+// "recede"   =>  "()()()"
+// "Success"  =>  ")())())"
+// "(( @"     =>  "))((" 
+// Notes
+// Assertion messages may be unclear about what they display in some languages. If you read "...It Should encode XXX", the "XXX" is the expected result, not the input!
+
+
+
+// My solution
+
+
+function duplicateEncode(word){
+    let diffWord = ''
+    
+    word = word.toLowerCase()
+    
+    for(let i = 0; i < word.length ; i++){
+      if(word.indexOf(word[i]) === word.lastIndexOf(word[i])){
+        diffWord+= "("
+      }else{
+        diffWord+= ")"
+      }
+    }
+   return diffWord
+ }
