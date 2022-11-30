8f2a1c5edef21b4b02ad7ec641f10a7d2012edf1
Noble Nwachukwu <ncnwachukwu@mix.wvu.edu>
2022-11-29 23:23:47 -0600

    added new codewars challenge

---
+++ 7kyu/str-arr-duplicates.js
@@ -0,0 +1,20 @@
+// DESCRIPTION:
+// In this Kata, you will be given an array of strings and your task is to remove all consecutive duplicate letters from each string in the array.
+
+// For example:
+
+// dup(["abracadabra","allottee","assessee"]) = ["abracadabra","alote","asese"].
+
+// dup(["kelless","keenness"]) = ["keles","kenes"].
+
+// Strings will be lowercase only, no spaces. See test cases for more examples.
+
+// Good luck!
+
+// My solution
+
+
+function dup(arr) {
+    return arr.map(x=> x.split('').filter((e,i,a)=> e !== a[i-1]).join(''))
+                                               
+   };
